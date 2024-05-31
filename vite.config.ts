7e4e57d88ecc21183d2850ebf6ad25fa0eb433ef
Saulo Candeira <saulocandeira@hotmail.com>
2024-05-31 16:46:42 -0300

    Add:

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/projects/' // Altere <REPO> para o nome do seu repositório, se estiver implantando no GitHub Pages sob um subdiretório
+  base: '/projects' // Altere <REPO> para o nome do seu repositório, se estiver implantando no GitHub Pages sob um subdiretório
 });
